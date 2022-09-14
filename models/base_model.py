70678b3c1f32700db3bb0c7d3856d972f2c63990
ggakila <gisorehaggai@gmail.com>
2022-09-14 22:59:42 +0300

    edits

--- models/base_model.py
+++ models/base_model.py
@@ -1,90 +1,75 @@
 #!/usr/bin/python3
-"""This is the base model class for AirBnB"""
-import models
+"""
+Contains class BaseModel
+"""
+
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+import models
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, DateTime
-from os import environ
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
-Base = declarative_base()
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
-s = "HBNB_TYPE_STORAGE"
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
 class BaseModel:
-    """This class will defines all common attributes/methods
-    for other classes
-    """
-    if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-        id = Column(
-            String(60),
-            unique=True,
-            nullable=False,
-            primary_key=True,
-            default=str(
-                uuid.uuid4()))
-        created_at = Column(
-            DateTime,
-            nullable=False,
-            default=datetime.utcnow())
-        updated_at = Column(
-            DateTime,
-            nullable=False,
-            default=datetime.utcnow())
+    """The BaseModel class from which future classes will be derived"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        '''
-        Instantiation of base model class
-        '''
-        cs = "HBNB_TYPE_STORAGE"
+        """Initialization of the base model"""
         if kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        elif cs not in environ.keys() or environ["HBNB_TYPE_STORAGE"] != "db":
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        '''
-        returns a string
-        '''
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        '''
-        representaion
-        '''
-        return self.__str__()
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        '''
-        updates updated_at to current
-        '''
-        self.updated_at = datetime.now()
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        '''
-        creates dictionary of the class
-        '''
-        my_dict = dict(self.__dict__)
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        if "_sa_instance_state" in my_dict.keys():
-            del my_dict["_sa_instance_state"]
-            models.storage.save()
-        return my_dict
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
 
     def delete(self):
+        """delete the current instance from the storage"""
         models.storage.delete(self)
-        models.storage.save()
--- models/city.py
+++ models/city.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python3
-"""This is the city class"""
+#!/usr/bin/python
+""" holds class City"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import environ
-from uuid import uuid4
 
-s = "HBNB_TYPE_STORAGE"
-if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class City(BaseModel, Base):
-        '''
-        This is the class for City Attributes
-        '''
-        __tablename__ = "cities"
+
+class City(BaseModel, Base):
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all,delete")
-
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class City(BaseModel):
-        '''
-        This is the class for City
-        '''
+        places = relationship("Place", backref="cities")
+    else:
         state_id = ""
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
